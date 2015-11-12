f2ee37d8be757d2f3e62e147293cc320898b9ed5
Pankaj-Gupta <guptapankaj1993@gmail.com>
2015-11-13 00:34:58 +0530

    added more codes

--- affine.hs
+++ affine.hs
@@ -3,6 +3,7 @@ import Data.List
 import Data.Char (ord, chr)
 
 
+
 affineEncode :: Int -> Int -> String -> String
 affineEncode a b = map f
  	where f c = chr $ (a*(ord c - ord 'A')+b) `mod` 26 + ord 'A'
@@ -12,4 +13,10 @@ affineDecode a b = map f
  	where f c = chr $ (26 - a `mod` 26)*(ord c - ord 'A' - b) `mod` 26 + ord 'A'
 
 main :: IO()
-main = do putStrLn $ affineDecode 5 8 $ affineEncode 5 8 "AFFINECIPHER"
+main = do 
+	putStrLn $ affineDecode 5 8 $ affineEncode 5 8 "AFFINECIPHER"
+	data1 <- hGetContents =<< openFile "american-english" ReadMode
+	data2 <- hGetContents =<< openFile "cracklib-small" ReadMode
+	let bigData = sort $ (words data1) ++ (words data2)
+	print bigData
+	
