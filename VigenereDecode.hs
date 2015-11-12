d020453f5758241460586f8db304d73d72dffe29
ABHINAV MEHTA <abhinavmehta1993@gmail.com>
2015-11-13 01:34:34 +0530

    vigenere

---
+++ VigenereDecode.hs
@@ -0,0 +1,11 @@
+module VigenereDecode where
+
+import Data.Char (ord, chr)
+
+
+vigenereDecode :: String -> String -> String
+vigenereDecode _ [] = []
+vigenereDecode (t:ts) (x:xs)
+	| ord 'a' <= ord x && ord 'z' >= ord x = (chr $ (-ord t + ord x - ord 'a') `mod` 26 + ord 'a') : vigenereDecode ts xs
+    | ord 'A' <= ord x && ord 'Z' >= ord x = (chr $ (-ord t + ord x - ord 'A') `mod` 26 + ord 'A') : vigenereDecode ts xs
+    | True = x : vigenereDecode ts xs
