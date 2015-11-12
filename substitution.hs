187cb12fa6fb10bc8fc5697f3f2f5faeff061e67
Akshay <akshay30aug#gmail.com>
2015-11-12 15:49:05 +0530

    substitution freq

---
+++ substitution.hs
@@ -0,0 +1,63 @@
+import Data.List
+import System.IO
+import Data.List
+import Data.Char
+import Data.Function
+
+checkchar::[Char]->Char
+checkchar (x:xs) = x
+
+clearnoise :: [Char] -> [Char] -- as words function separates with space, there might be anomalies.
+clearnoise [] = []
+clearnoise (x:xs) | x=='.'	=clearnoise xs
+				   | x==','	=clearnoise xs
+				   | x=='!'	=clearnoise xs
+				   | x=='?'	=clearnoise xs
+				   | x==':'	=clearnoise xs
+				   | x==';'	=clearnoise xs
+				   | x=='"'	=clearnoise xs
+				   | x=='{'	=clearnoise xs
+				   | x=='}'	=clearnoise xs
+				   | x=='('	=clearnoise xs
+				   | x==')'	=clearnoise xs
+				   | x==' ' = clearnoise xs
+				   | otherwise = (x:(clearnoise xs))
+
+reduceredundancy:: [String]->String
+reduceredundancy [] = []
+reduceredundancy (x:xs) = (checkchar x): (reduceredundancy xs)
+
+lowerCase::String->String -- this makes the whole string in lower case so as to make "Hello" and "hello" same
+lowerCase [] = []
+lowerCase (x:xs) = (toLower x):(lowerCase xs)
+
+maketuple::[Int]->[Char]->[(Int,Char)]
+maketuple [] [] = []
+maketuple (x:xs) (y:ys) = ((x,y)):(maketuple xs ys)
+
+correcting::[String]->[String] -- this function calls clearnoise on every word
+correcting [] = []
+correcting (x:xs) = (clearnoise x):(correcting xs)
+
+beechka:: String->[String]
+beechka x = group (sort x)
+
+letterfrequency :: [String] -> [Int]
+letterfrequency x = fmap length (x)
+
+main::IO() -- main function
+main = do
+	inh <- openFile "input.txt" ReadMode
+	contents <- hGetContents inh
+	let samecasewords = lowerCase contents
+	let wextract = words samecasewords
+	let wfinal = correcting wextract
+	let sortedwords = groupBy ((==) `on` length) $ sortBy (compare `on` length) wfinal
+	let onlyalphabets = clearnoise samecasewords
+	let chunksof2 = sort (chunksOf 2 samecasewords)
+	let groupedsorted = beechka onlyalphabets
+	let frequencylist = letterfrequency groupedsorted
+	let alphabetlist = reduceredundancy groupedsorted
+	let tuples = maketuple frequencylist alphabetlist
+	let sortedtuples = reverse (sort tuples)
+	print sortedtuples
